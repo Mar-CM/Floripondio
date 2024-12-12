7b21decf46fa3fd1e54a85e7a04551900f8b87ba
Mar Caballero Moryno <mar.caballero@hotmail.es>
2024-12-12 18:15:13 +0100

    Create landing

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import "../scss/App.scss";
+
+import Landing from "./Landing.jsx";
+
+// import { Route, Routes, useLocation } from "react-router-dom";
+// import { useState, useEffect } from "react";
+
+function App() {
+	return (
+		<>
+			<Landing />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Landing.jsx
@@ -0,0 +1,11 @@
+function Landing() {
+	return (
+		<>
+			<header className="landing">
+				<h1 className="titleLanding">Floripondio</h1>
+			</header>
+		</>
+	);
+}
+
+export default Landing;
